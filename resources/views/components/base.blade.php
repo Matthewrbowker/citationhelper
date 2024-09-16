5bfccd9d8e29517718bf301d3248dd74b3653187
Matthew Bowker <matthewrbowker.bugs@gmail.com>
2024-09-16 10:42:12 -0600

    Add support for tools-static bootstrap

---
+++ resources/views/components/base.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<HTML lang="en">
+<HEAD>
+    <link href="https://tools-static.wmflabs.org/cdnjs/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://tools-static.wmflabs.org/cdnjs/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>{{ $title ?? "Matthew's Tools" }}</title>
+</HEAD>
+<Body>
+<div class="container">
+    {{ $content ?? "PLEASE CONFIGURE BLADE" }}
+</div>
+</Body>
+</HTML>
